028ad087424e67569d88198bfcd710d4c9ef2d0b
carolpinheiro <ommasshi@gmail.com>
2021-09-26 21:22:26 -0300

    Log de batalha adicionado

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -8,7 +8,8 @@ const app = Vue.createApp({
             monsterHealth: 100,
             playerHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         }
     },
     computed: {
@@ -47,29 +48,31 @@ const app = Vue.createApp({
     },
     methods: {
         attackMonster() {
-            console.log(this.monsterHealth, this.playerHealth)
             this.currentRound++
             const attackValue = getRandomValue(5, 12)
             this.monsterHealth -= attackValue
             this.attackPlayer()
-
+            this.addLogMessage('player', 'attack', attackValue)
         },
         attackPlayer() {
             this.currentRound++
             const attackValue = getRandomValue(8, 17)
             this.playerHealth -= attackValue
+            this.addLogMessage('monster', 'attack', attackValue)
         },
         specialAttackMonster() {
             this.currentRound++
             const attackValue = getRandomValue(10, 25)
             this.monsterHealth -= attackValue
             this.attackPlayer()
+            this.addLogMessage('player', 'special-attack', attackValue)
         },
         newGame() {
             this.monsterHealth = 100
             this.playerHealth = 100
             this.winner = null
             this.currentRound = 0
+            this.logMessages = []
         },
         healPlayer() {
             this.currentRound++
@@ -80,9 +83,17 @@ const app = Vue.createApp({
                 this.playerHealth += healValue;
             }
             this.attackPlayer()
+            this.addLogMessage('player', 'heal', healValue)
         },
         surrender() {
             this.winner = 'monster'
+        },
+        addLogMessage(who, what, value) {
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value
+            })
         }
     },
 })
