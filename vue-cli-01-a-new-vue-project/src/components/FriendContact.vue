9983591a5acbd01dec3284e8da80683b82bc743d
Carol <ommasshi@gmail.com>
2021-09-28 17:53:15 -0300

    props adicionadas ao componente friends

--- vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
+++ vue-cli-01-a-new-vue-project/src/components/FriendContact.vue
@@ -1,16 +1,17 @@
 <template>
   <li>
-    <h2>{{ friend.name }}</h2>
+    <h2>{{ name }}</h2>
     <button @click="toggleButtons">Show Details</button>
     <ul v-if="toggleButton">
-      <li><strong>Phone:</strong> {{ friend.phone }}</li>
-      <li><strong>E-mail:</strong> {{ friend.email }}</li>
+      <li><strong>Phone:</strong> {{ phoneNumber }}</li>
+      <li><strong>E-mail:</strong> {{ emailAddress }}</li>
     </ul>
   </li>
 </template>
 
 <script>
 export default {
+  props: ["name", "phoneNumber", "emailAddress"],
   data() {
     return {
       friend: {
