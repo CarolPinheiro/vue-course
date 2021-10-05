5645a83c60d8a803ea88ae03561a7b5f061afa9b
Carol <ommasshi@gmail.com>
2021-10-05 09:40:06 -0300

    Segundo projeto de meio do curso adicionado

---
+++ prj-cmp-01-starting-setup/src/components/learningresources/TheResources.vue
@@ -0,0 +1,84 @@
+<template>
+  <span>
+    <base-card>
+      <base-button
+        @click="setSelectedTab('stored-resources')"
+        :mode="storedResButtonMode"
+      >
+        Stored Resources</base-button
+      >
+      <base-button
+        @click="setSelectedTab('add-resource')"
+        :mode="addResButtonMode"
+        >Add Resource</base-button
+      >
+    </base-card>
+    <keep-alive>
+      <component :is="selectedTab"></component>
+    </keep-alive>
+  </span>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    }
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'official-google',
+          title: 'Official Google',
+          description: 'The official Google',
+          link: 'https://google.com'
+        }
+      ]
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, link) {
+      this.storedResources.unshift({
+        id: new Date(),
+        title,
+        description,
+        link
+      });
+
+      this.selectedTab = 'stored-resources';
+    },
+    deleteResource(id) {
+      const index = this.storedResources.findIndex(e => e.id === id);
+      this.storedResources.splice(index, 1);
+    }
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+      deleteResource: this.deleteResource
+    };
+  }
+};
+</script>
