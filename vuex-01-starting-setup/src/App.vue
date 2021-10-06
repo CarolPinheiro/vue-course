86001522644c668dd815d38e0375f815257bf713
Carol <ommasshi@gmail.com>
2021-10-06 11:33:33 -0300

    payload e mutations adicionado junto aos states

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <base-container title="Vuex">
+    <p>{{ counter }}</p>
+    <button @click="addOne">Add</button>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+
+export default {
+  components: {
+    BaseContainer
+  },
+  computed: {
+    counter() {
+      return this.$store.state.counter;
+    }
+  },
+  methods: {
+    addOne() {
+      this.$store.commit({ type: 'increase', value: 2 });
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ vuex-01-starting-setup/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
+
+import App from './App.vue';
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0
+    };
+  },
+  mutations: {
+    increase(state, payload) {
+      state.counter = state.counter + payload.value;
+    }
+  }
+});
+
+const app = createApp(App);
+
+app.use(store);
+
+app.mount('#app');
